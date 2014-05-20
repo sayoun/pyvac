e56a8b5cf4e298d69bc321a4ce323e2f881900d0
sayoun <sayoun@gandi.net>
2014-05-20 10:13:42 +0200

    Sort vacation type by id when displaying on home page

--- pyvac/views/__init__.py
+++ pyvac/views/__init__.py
@@ -17,7 +17,7 @@ class Home(RedirectView):
 
         ret_dict = {'types': [vac.name for vac in
                               VacationType.find(self.session,
-                                                order_by=[VacationType.name])]}
+                                                order_by=[VacationType.id])]}
         if self.request.matched_route:
             matched_route = self.request.matched_route.name
             ret_dict.update({'matched_route': matched_route,
