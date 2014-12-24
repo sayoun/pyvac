c145552b97c3c3151fd71741962855302d096136
sayoun <sayoun@gandi.net>
2014-12-24 11:45:56 +0100

    Update changelogs and bump version to 1.2

--- pyvac/__init__.py
+++ pyvac/__init__.py
@@ -12,7 +12,7 @@ from .models import create_engine
 from .helpers.i18n import locale_negotiator
 from .helpers.authentication import RouteSwithchAuthPolicy
 
-__version__ = '1.1'
+__version__ = '1.2'
 
 
 def main(global_config, **settings):
