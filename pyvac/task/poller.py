bef10074eb77e2ecba0bc693dfded3401501dd73
sayoun <sayoun@gandi.net>
2015-01-02 11:38:20 +0100

    Disable the PENDING notification reminder mail as it seems to bugs

--- pyvac/task/poller.py
+++ pyvac/task/poller.py
@@ -52,14 +52,14 @@ class Poller(Task):
         self.log.info('number of ACCEPTED_NOTIFIED requests: %d' %
                       len(req_accepted_notified))
 
-        req_pending_notified = Request.by_status(session, 'PENDING',
-                                                 notified=True)
-        self.log.info('number of PENDING_NOTIFIED requests: %d' %
-                      len(req_pending_notified))
+        # req_pending_notified = Request.by_status(session, 'PENDING',
+        #                                          notified=True)
+        # self.log.info('number of PENDING_NOTIFIED requests: %d' %
+        #               len(req_pending_notified))
 
         req_list = []
         req_list.extend(req_accepted_notified)
-        req_list.extend(req_pending_notified)
+        # req_list.extend(req_pending_notified)
 
         for req in req_list:
             self.log.info('selecting task for req type %r' % req.status)
@@ -67,8 +67,8 @@ class Poller(Task):
             check_status = req.status
             if req.status == 'ACCEPTED_MANAGER' and req.notified:
                 check_status = 'ACCEPTED_NOTIFIED'
-            if req.status == 'PENDING_NOTIFIED' and req.notified:
-                check_status = 'PENDING_NOTIFIED'
+            # if req.status == 'PENDING' and req.notified:
+            #     check_status = 'PENDING_NOTIFIED'
 
             req_task = self.worker_tasks[check_status]
             self.log.info('task selected %r' % req_task.name)
