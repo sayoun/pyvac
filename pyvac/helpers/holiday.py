f02ee7d3877c995b0b936618756644964b48f54f
sayoun <sayoun@gandi.net>
2015-12-22 15:44:32 +0100

    Fixes zh holidays list for 2016
    The holiday dates are changed by the Taiwanese governement each October
    so we must override workalendar return list to use the correct ones.

--- pyvac/helpers/holiday.py
+++ pyvac/helpers/holiday.py
@@ -19,6 +19,24 @@ conv_table = {
 }
 
 
+override = {'zh': {2016: [('2016-01-01', 'New Years Day'),
+                          ('2016-02-07', 'Chinese New Years Eve'),
+                          ('2016-02-08', 'Chinese New Years Day'),
+                          ('2016-02-09', 'Chinese New Year Holiday 1'),
+                          ('2016-02-10', 'Chinese New Year Holiday 2'),
+                          ('2016-02-11', 'Chinese New Year Holiday 3'),
+                          ('2016-02-12', 'Chinese New Year Holiday 4'),
+                          ('2016-02-29', '228 Memorial Day (observed)'),
+                          ('2016-04-04', 'Childrens Day'),
+                          ('2016-04-05', 'Tomb Sweeping Day'),
+                          ('2016-06-09', 'Dragon Boat Festival'),
+                          ('2016-09-15', 'Mid-Autumn Festival'),
+                          ('2016-09-15', 'Mid-Autumn Festival'),
+                          ('2016-09-16', 'Mid-Autumn Festival (observance)'),
+                          ('2016-10-10', 'National Day')]},
+            }
+
+
 def utcify(date):
     """ return an UTC datetime from a Date object """
     return calendar.timegm(date.timetuple()) * 1000
@@ -38,6 +56,16 @@ def get_holiday(user, year=None, use_datetime=False):
     # retrieve Dates from workalendar
     holiday_current_raw = [dt for dt, _ in cal.holidays(current_year)]
     holiday_next_raw = [dt for dt, _ in cal.holidays(next_year)]
+
+    if user.country in override and current_year in override[user.country]:
+        holiday_current_raw = [datetime.strptime(dt, '%Y-%m-%d')
+                               for dt, _ in
+                               override[user.country][current_year]]
+
+    if user.country in override and next_year in override[user.country]:
+        holiday_next_raw = [datetime.strptime(dt, '%Y-%m-%d')
+                            for dt, _ in
+                            override[user.country][next_year]]
 
     if not use_datetime:
         # must cast to javascript timestamp
