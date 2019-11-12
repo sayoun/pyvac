2c058f288f20dbc0d05c740c5bdbef3eb3a1f5d2
sayoun <dejan.filipovic@gmail.com>
2019-11-12 23:38:06 +0100

    New release 1.9

--- pyvac/__init__.py
+++ pyvac/__init__.py
@@ -12,7 +12,7 @@ from .models import create_engine
 from .helpers.i18n import locale_negotiator
 from .helpers.authentication import RouteSwithchAuthPolicy
 
-__version__ = '1.8'
+__version__ = '1.9'
 
 
 def main(global_config, **settings):
